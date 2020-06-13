8b6e51b53be30eadb94a4e23627067e1ddbee229
hecklerjim <59577337+hecklerjim@users.noreply.github.com>
2020-06-12 21:24:36 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+// CONTENT LOADERS
+
+function navLoader(){
+  $(function(){
+    $("#nav-placeholder").load("nav.html");
+  });
+  }
+  
+  function footerLoader(){
+    $(function(){
+      $("#footer-placeholder").load("footer.html");
+    });
+    }
