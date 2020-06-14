b1624ef6a4635d6523774ac484ab6ca7d756e936
hecklerjim <59577337+hecklerjim@users.noreply.github.com>
2020-06-13 17:30:59 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -6,20 +6,27 @@ function navLoader(){
   });
   }
   
-  function footerLoader(){
-    $(function(){
-      $("#footer-placeholder").load("footer.html");
-    });
-    }
+function footerLoader(){
+  $(function(){
+    $("#footer-placeholder").load("footer.html");
+  });
+  }
 
 // SLIDING NAVBAR
 
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 200) {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 180) {
     document.getElementById("navbar-block").style.top = "0";
   } else {
     document.getElementById("navbar-block").style.top = "-60px";
   }
+}
+
+// COPY FUNCTION
+
+function copiedToClipboardEmail() {
+  document.getElementById('alertEmail').innerHTML = '<i class="fa fa-check" aria-hidden="true"></i> Email Copied!';
+  setTimeout(function(){document.getElementById('alertEmail').innerHTML = '<i class="fa fa-link" aria-hidden="true"></i> Copy Email Again';}, 5000);
 }
