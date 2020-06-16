3272029a25ead9c8e52af0edf6be4e2c3511c778
hecklerjim <59577337+hecklerjim@users.noreply.github.com>
2020-06-15 19:18:18 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ function footerLoader(){
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 180) {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.getElementById("navbar-block").style.top = "0";
   } else {
     document.getElementById("navbar-block").style.top = "-60px";
